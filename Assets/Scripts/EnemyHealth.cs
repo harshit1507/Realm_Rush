0ac490d23d84c2115ef082baa7b48e5622a4f37a
harshit1507 <harshit1507@yahoo.com>
2021-06-06 23:34:54 +0530

    increase difficulty on enemy death

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -3,9 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
+    [Tooltip("Adds amount to maxHitPoints when enemy  dies.")]
+    [SerializeField] int difficultyRamp = 1;
+
     int currentHitPoints = 0;
 
     Enemy enemy;
@@ -30,6 +34,7 @@ public class EnemyHealth : MonoBehaviour
         if(currentHitPoints <= 0)
         {
             gameObject.SetActive(false);
+            maxHitPoints += difficultyRamp;
             enemy.RewardGold();
         }
     }
